2e80d89c34107725f77b6c595f3bc50f1d3c84d1
chitkhine <ck.light@gmail.com>
2023-06-17 01:39:55 +0700

    :sparkles: add brand and category to product

---
+++ TheSaverAPI/Model/ProductBrand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace TheSaverAPI.Model
+{
+	public class ProductBrand
+	{
+        public int ProductId { get; set; }
+
+        public int BrandId { get; set; }
+
+        public Product Product { get; set; }
+
+        public Brand Brand { get; set; }
+    }
+}
+
